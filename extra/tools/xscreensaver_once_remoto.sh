d3227f5c4488a7fb8d0357425d054050c729526e
Sebastian Pastor <sebastian.pastor@zigor.com>
2020-12-18 13:54:16 +0100

    Salvapantallas: version ZGR

--- extra/tools/xscreensaver_once_remoto.sh
+++ extra/tools/xscreensaver_once_remoto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-su - genjur -c "DISPLAY=:1 xv -root -rmode 5 -random -viewonly  -wloop /home/genjur/SE_Logo_PanelPC_waiting.jpg"
+su - genjur -c "DISPLAY=:1 xv -root -rmode 5 -random -viewonly  -wloop /home/genjur/ZGR_Logo_Waiting.jpg"
